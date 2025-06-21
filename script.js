fc3dd5e5234349cfa1f25305743ab7eb853ae18e
Dardeiya <dardeiyaokbro@gmail.com>
2025-06-21 22:36:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let clickCount = 0;
+    const updatesButton = document.getElementById('updates-button');
+    const cheatInfoSection = document.getElementById('cheat-info');
+
+    updatesButton.addEventListener('click', function() {
+        clickCount++;
+        
+        if (clickCount === 7) {
+            cheatInfoSection.style.display = 'block'; // Показать скрытый раздел после 7 кликов
+            clickCount = 0; // Сбросить счётчик
+        }
+    });
+});
